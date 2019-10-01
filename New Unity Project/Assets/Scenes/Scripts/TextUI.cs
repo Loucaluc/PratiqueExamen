64ec52c5749b0dca4d5a849d253fb25e9a83da9e
B533P-12\533 <loucaluc@live.fr>
2019-10-01 11:36:42 -0400

    Ajout du textUI

---
+++ New Unity Project/Assets/Scenes/Scripts/TextUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextUI : MonoBehaviour {
+
+    public GameObject text;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = FindObjectOfType<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Fire1") != 0)
+        {
+            Instantiate(text,transform);
+            System.Threading.Thread.Sleep(2000);
+            audioSource.Play();
+        }
+    }
+}   
