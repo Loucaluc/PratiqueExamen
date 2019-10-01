80db837ce8b096c1cc5f53f99fc90890191d1032
B533P-12\533 <loucaluc@live.fr>
2019-10-01 10:17:30 -0400

    Fin avion question 1

---
+++ New Unity Project/Assets/Scenes/Scripts/HeliceRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliceRotation : MonoBehaviour {
+
+    // Use this for initialization
+    private float rotationSpeed = 0;
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        gameObject.transform.Rotate(new Vector3(0, 0, Time.deltaTime * rotationSpeed * 1000));
+    }
+
+    public void DefinirRotationSpeed(float vitesse)
+    {
+        rotationSpeed = vitesse;
+    }
+}
---
+++ New Unity Project/Assets/Scenes/Scripts/PlaneMouvement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlaneMouvement : MonoBehaviour {
+
+    Rigidbody rb;
+    public int speedFactor = 400;
+    private HeliceRotation heliceRotation;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = FindObjectOfType<Rigidbody>();
+        heliceRotation = GetComponentInChildren<HeliceRotation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Jump") != 0)
+        {
+            rb.AddForce(new Vector3(0, Input.GetAxis("Jump") * Time.deltaTime * speedFactor, 0));
+        }
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            rb.AddForce(new Vector3(Input.GetAxis("Vertical") * Time.deltaTime * speedFactor, 0, 0));
+        }
+
+        heliceRotation.DefinirRotationSpeed(rb.velocity.magnitude);
+    }
+}
